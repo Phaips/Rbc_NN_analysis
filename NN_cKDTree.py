abfa8c88f96e77986aeff08ec4e9b4595d691508
Phaips <103127272+Phaips@users.noreply.github.com>
2023-06-20 13:26:07 +0200

    Update NN_cKDTree.py

--- NN_cKDTree.py
+++ NN_cKDTree.py
@@ -19,14 +19,9 @@ pixel_size = 7.84  # bin4 pixel size for example
 k_values = range(2, 13)  # 12 NN's (can set to k=2 for NN)
 
 distances_dict = {}
-mean_distances = {}
-median_distances = {}
 
 for key, df in enumerate(dataframes):
     coords = df[['orig_x', 'orig_y', 'orig_z']].values * pixel_size
     tree = cKDTree(coords)
     distances, neighbors = tree.query(coords, k=max(k_values))
     distances_dict[key] = {f'distance_k{k}': distances[:, k-1] for k in k_values}
-
-   # mean_distances[key] = {k: np.mean(distances_dict[key][f'distance_k{k}']) for k in k_values}
-   # median_distances[key] = {k: np.median(distances_dict[key][f'distance_k{k}']) for k in k_values}
