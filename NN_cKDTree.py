36e9e762a2b37e80900a40a31c4139bec48eb797
Phaips <103127272+Phaips@users.noreply.github.com>
2023-06-20 13:17:49 +0200

    Update NN_cKDTree.py

--- NN_cKDTree.py
+++ NN_cKDTree.py
@@ -5,9 +5,19 @@ from scipy.spatial import cKDTree
 
 # Dataframes contain .star files from template matching as df
 
+# For example like this: 
+# dataframes = {}
+# for file_number, file_path in zip(file_numbers, path):
+#    df = starfile.read(file_path)
+#    dataframes[f"df{file_number}"] = df
+
+# Initialize tree
 trees = {}
-pixel_size = 7.84  # bin4 pixel size
-k_values = range(2, 13)  # 12 NN
+
+# Set tomo pixel size and number of nearest neighbors (NN)
+pixel_size = 7.84  # bin4 pixel size for example
+k_values = range(2, 13)  # 12 NN's (can set to k=2 for NN)
+
 distances_dict = {}
 mean_distances = {}
 median_distances = {}
@@ -18,20 +28,5 @@ for key, df in enumerate(dataframes):
     distances, neighbors = tree.query(coords, k=max(k_values))
     distances_dict[key] = {f'distance_k{k}': distances[:, k-1] for k in k_values}
 
-    mean_distances[key] = {k: np.mean(distances_dict[key][f'distance_k{k}']) for k in k_values}
-    median_distances[key] = {k: np.median(distances_dict[key][f'distance_k{k}']) for k in k_values}
-
-
-# Angular analysis
-
-dfcos = {}
-
-for key, df in enumerate(dataframes):
-    cos_values = []
-    for i, neighbors_indices in enumerate(neighbors):
-        particle_angles = df.loc[[i], ['phi', 'psi', 'the']].values
-        neighbor_angles = df.loc[neighbors_indices[1:], ['phi', 'psi', 'the']].values  # Exclude the particle itself
-        cos_angles = np.cos(np.deg2rad(neighbor_angles - particle_angles))
-        cos_values.append(cos_angles)
-    dfcos[key] = cos_values
-
+   # mean_distances[key] = {k: np.mean(distances_dict[key][f'distance_k{k}']) for k in k_values}
+   # median_distances[key] = {k: np.median(distances_dict[key][f'distance_k{k}']) for k in k_values}
