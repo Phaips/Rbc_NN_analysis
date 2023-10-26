5efbc2cb821b7aac4257bedaa01fd58de29a4451
Philippe Van der Stappen <103127272+Phaips@users.noreply.github.com>
2023-10-26 13:42:56 +0200

    Create compactNN.py

---
+++ compactNN.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import starfile
+from scipy.spatial import cKDTree
+import seaborn as sns
+
+pixel_size = 10
+k_values = range(2, 6)
+
+colormap = plt.get_cmap('tab10')
+colors = [colormap(i) for i in range(len(k_values))]
+
+path = "/scicore/home/engel0006/vysamu91/chlorellaRbc/pytom/PyTOM/all_motl/particles.star"
+tomo = starfile.read(path)
+
+dataframes = {tomo_num: df.reset_index(drop=True) for tomo_num, df in tomo.groupby('ptmMicrographName')}
+
+distances_dict = {}
+for key, df in dataframes.items():
+    coords = df[['ptmCoordinateX', 'ptmCoordinateY', 'ptmCoordinateZ']].values * pixel_size
+    tree = cKDTree(coords)
+    distances, _ = tree.query(coords, k=max(k_values))
+    
+    distances_dict[key] = {f'distance_k{k}': distances[:, k-1] for k in k_values}
+
+fig, axes = plt.subplots(len(dataframes), 4, figsize=(20, 4 * len(dataframes)))
+
+for idx, key in enumerate(distances_dict):
+    ax_distance = axes[idx, 0]
+    for k in k_values:
+        distances_k = distances_dict[key][f'distance_k{k}']
+        sns.kdeplot(distances_k, fill=True, color=colors[k-2], ax=ax_distance, label=f'k={k}: {np.mean(distances_k):.2f}Å ± {np.std(distances_k):.2f}Å')
+        
+    ax_distance.set_xlabel('Distance (Å)')
+    ax_distance.set_ylabel('Density')
+    ax_distance.set_title(f'Distances for tomo {key}')
+    ax_distance.set_xlim(20, 500)
+    ax_distance.legend()
+    
+    angle_columns = ['ptmAngleRot', 'ptmAngleTilt', 'ptmAnglePsi']
+    angle_names = ['Rot', 'Tilt', 'Psi']
+    for angle_idx, angle_col in enumerate(angle_columns):
+        ax_angle = axes[idx, angle_idx + 1]
+        dataframes[key][angle_col].hist(ax=ax_angle, bins=30, color=colors[angle_idx], alpha=0.7)
+        ax_angle.set_title(f'{angle_names[angle_idx]} Angle for tomo {key}')
+        ax_angle.set_xlabel('Angle (degrees)')
+        ax_angle.set_ylabel('Count')
+
+plt.tight_layout()
+plt.show()
