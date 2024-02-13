0d88e568a426dc25c9f308cbab935af54871d286
Philippe Van der Stappen <103127272+Phaips@users.noreply.github.com>
2024-02-13 19:44:02 +0100

    Update nearest_neighbor.py

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -11,7 +11,8 @@ Give path to your starfile motive/particle list
 path = "/.../all_motl/particles.star"
 tomo = starfile.read(path)
 
-dataframes = {tomo_num: df.reset_index(drop=True) for tomo_num, df in tomo.groupby('ptmMicrographName')} # Adjust accordingly to your motive list (i.e. rlnMicrographName or tomo_num)
+dataframes = {tomo_num: df.reset_index(drop=True) 
+              for tomo_num, df in tomo.groupby('ptmMicrographName')} # Adjust accordingly to your motive list (i.e. rlnMicrographName or tomo_num)
 
 # Pixel/Voxel size at your tomogram binning
 pixel_size = 7.84
@@ -34,7 +35,11 @@ for idx, key in enumerate(distances_dict):
     ax_distance = axes[idx, 0]
     for k in k_values:
         distances_k = distances_dict[key][f'distance_k{k}']
-        sns.kdeplot(distances_k, fill=True, color=colors[k-2], ax=ax_distance, label=f'k={k}: {np.mean(distances_k):.2f}Å ± {np.std(distances_k):.2f}Å')
+        sns.kdeplot(distances_k, 
+                    fill=True, 
+                    color=colors[k-2],
+                    ax=ax_distance, 
+                    label=f'k={k}: {np.mean(distances_k):.2f}Å ± {np.std(distances_k):.2f}Å')
         
     ax_distance.set_xlabel('Distance (Å)')
     ax_distance.set_ylabel('Density')
@@ -46,7 +51,10 @@ for idx, key in enumerate(distances_dict):
     angle_names = ['Rot', 'Tilt', 'Psi'] # Also this depends on the voncention! sometimes Phi or Psi are switched or differently labeled!
     for angle_idx, angle_col in enumerate(angle_columns):
         ax_angle = axes[idx, angle_idx + 1]
-        dataframes[key][angle_col].hist(ax=ax_angle, bins=30, color=colors[angle_idx], alpha=0.7)
+        dataframes[key][angle_col].hist(ax=ax_angle, 
+                                        bins=30, 
+                                        color=colors[angle_idx], 
+                                        alpha=0.7)
         ax_angle.set_title(f'{angle_names[angle_idx]} Angle for tomo {key}')
         ax_angle.set_xlabel('Angle (degrees)')
         ax_angle.set_ylabel('Count')
